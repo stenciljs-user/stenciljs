20e5903fdf21a2d2d9955ce85f40862df72b670a
stenciljs-user <tejasgirasepic@gmail.com>
2025-07-01 01:47:33 -0600

    Update stenciljs-main.js

--- stenciljs-main.js
+++ stenciljs-main.js
@@ -1,6 +1,15 @@
-import { Component, h, State, Prop, Watch } from '@stencil/core';
+import {
+  Component,
+  h,
+  State,
+  Prop,
+  Watch,
+} from '@stencil/core';
 import { createMerchantTransactionSetup } from '../../services/merchantTransactionSetup/createMerchantTransactionSetup';
-import { AlertMessageType, PaymentStatus } from '../../utils/enums';
+import {
+  AlertMessageType,
+  PaymentStatus,
+} from '../../utils/enums';
 import { CONSTANTS } from '../../utils/constant';
 import type {
   BillingAddress,
@@ -14,60 +23,40 @@ import type {
   shadow: false,
 })
 export class PaymentProcessing {
-  @Prop() sourceMerchantId: string;
-  @Prop() partnerId: string;
+  /**
+   * Configuration Props
+   */
+  @Prop() sourceMerchantId!: string;
+  @Prop() partnerId!: string;
   @Prop() totalAmount: number = 10;
-  @Prop() transactionReferenceId: string;
-  @Prop() primaryColor: string;
-  @Prop() buttonTextColor: string;
-  @Prop() borderRadius: string;
-  @Prop() orderSummary: string;
-  @Prop() billingAddress: string;
-  @Prop() shippingAddress: string;
+  @Prop() transactionReferenceId!: string;
+  @Prop() primaryColor?: string;
+  @Prop() buttonTextColor?: string;
+  @Prop() borderRadius?: string;
+  @Prop() orderSummary!: string;
+  @Prop() billingAddress?: string;
+  @Prop() shippingAddress?: string;
 
-  @State() transactionSetupResponse: TransactionSetupResponse;
-  @State() transactionStatus: PaymentStatus;
+  /**
+   * State Management
+   */
+  @State() transactionSetupResponse?: TransactionSetupResponse;
+  @State() transactionStatus?: PaymentStatus;
   @State() loading: boolean = false;
   @State() error: boolean = false;
   @State() disabledPaymentMethod: boolean = true;
   @State() hasSameBillingAddressChecked: boolean = false;
-  @State() paymentMethod: string;
+  @State() paymentMethod?: string;
 
-  private setLoading = (isLoading: boolean) => {
-    this.loading = isLoading;
-  };
-
-  private setError = (isError: boolean) => {
-    this.error = isError;
-  };
-
+  /**
+   * Watcher for setting up hosted payment page when setup response updates
+   */
   @Watch('transactionSetupResponse')
-  private setupHostedPaymentPage() {
-    this.setLoading(true);
-    if (window['WPCL'] && this.transactionSetupResponse?.transactionSetupUrl) {
-      const library = new window['WPCL'].Library();
-      library.setup({
-        url: this.transactionSetupResponse.transactionSetupUrl,
-        type: 'iframe',
-        inject: 'immediate',
-        target: 'hpp-payment-component',
-        accessibility: true,
-        debug: true,
-        language: 'en',
-        resultCallback: async (result: HppTransactionResponse) => {
-          this.transactionStatus = result.order.status;
-          if (this.transactionStatus === PaymentStatus.SESSION_EXPIRED) {
-            await this.loadTransactionSetup();
-            this.setupHostedPaymentPage();
-          }
-        },
-      });
-      this.setLoading(false);
-    }
+  handleTransactionSetupChange(): void {
+    this.setupHostedPaymentPage();
   }
 
-  componentWillLoad() {
-    // Check if passed billing address valid and enabled payment method
+  componentWillLoad(): void {
     this.disabledPaymentMethod = !this.hasValidBillingAddress;
   }
 
@@ -75,18 +64,19 @@ export class PaymentProcessing {
     this.loadHostedPaymentLibrary();
   }
 
-  private get hasValidBillingAddress() {
-    return !!(
-      this.billingAddress &&
-      typeof this.billingAddress === 'string' &&
-      JSON.parse(this.billingAddress) &&
-      this.isValidBillingAddress(JSON.parse(this.billingAddress))
-    );
+  private get hasValidBillingAddress(): boolean {
+    if (!this.billingAddress) return false;
+    try {
+      const parsed = JSON.parse(this.billingAddress);
+      return this.isValidBillingAddress(parsed);
+    } catch (err) {
+      return false;
+    }
   }
 
-  private isValidBillingAddress(obj: any) {
-    return !!(
-      obj &&
+  private isValidBillingAddress(obj: any): boolean {
+    return (
+      !!obj &&
       typeof obj?.billingAddress1 === 'string' &&
       typeof obj?.billingCity === 'string' &&
       typeof obj?.billingState === 'string' &&
@@ -94,146 +84,171 @@ export class PaymentProcessing {
     );
   }
 
-  private async loadTransactionSetup() {
-    this.transactionSetupResponse = await createMerchantTransactionSetup({
-      totalAmount: this.totalAmount,
-      referenceId: this.transactionReferenceId,
-      // TODO: uncomment and send as part of payload
-      billingAddress: this.hasValidBillingAddress
-        ? JSON.parse(this.billingAddress)
-        : (this.billingAddress as BillingAddress),
-      // shippingAddress: this.shippingAddress,
-      styleConfig: {
-        primaryColor: this.primaryColor,
-        buttonTextColor: this.buttonTextColor,
-        borderRadius: this.borderRadius,
-      },
-      sourceMerchantId: this.sourceMerchantId,
-      partnerId: this.partnerId,
-      setLoading: this.setLoading,
-      setError: this.setError,
-    });
-    // this.setupHostedPaymentPage();
+  private setLoading = (isLoading: boolean): void => {
+    this.loading = isLoading;
+  };
+
+  private setError = (isError: boolean): void => {
+    this.error = isError;
+  };
+
+  private async loadTransactionSetup(): Promise<void> {
+    try {
+      const billingParsed: BillingAddress = this.hasValidBillingAddress
+        ? JSON.parse(this.billingAddress!)
+        : ({} as BillingAddress);
+
+      this.transactionSetupResponse = await createMerchantTransactionSetup({
+        totalAmount: this.totalAmount,
+        referenceId: this.transactionReferenceId,
+        billingAddress: billingParsed,
+        styleConfig: {
+          primaryColor: this.primaryColor,
+          buttonTextColor: this.buttonTextColor,
+          borderRadius: this.borderRadius,
+        },
+        sourceMerchantId: this.sourceMerchantId,
+        partnerId: this.partnerId,
+        setLoading: this.setLoading,
+        setError: this.setError,
+      });
+    } catch (err) {
+      this.setError(true);
+    }
   }
 
-  private loadHostedPaymentLibrary() {
+  private loadHostedPaymentLibrary(): void {
     this.setLoading(true);
     const script = document.createElement('script');
     script.src =
       'https://payments.worldpay.com/resources/hpp/integrations/embedded/js/hpp-embedded-integration-library.js';
     script.async = true;
-    script.onload = () => {
-      this.setLoading(false);
-    };
-    script.onerror = function () {
-      // Code to execute if the script fails to load
-      this.setLoading(false);
-      this.setError(true);
-    };
-
+    script.onload = () => this.setLoading(false);
+    script.onerror = () => this.setError(true);
     document.head.appendChild(script);
   }
 
-  // TODO: Add detailed error handling and user-friendly messages for various transaction failure scenarios.
-  private getStatusAlertMessage() {
-    let message = '';
+  private setupHostedPaymentPage(): void {
+    if (!window['WPCL'] || !this.transactionSetupResponse?.transactionSetupUrl)
+      return;
+
+    const library = new window['WPCL'].Library();
+    library.setup({
+      url: this.transactionSetupResponse.transactionSetupUrl,
+      type: 'iframe',
+      inject: 'immediate',
+      target: 'hpp-payment-component',
+      accessibility: true,
+      debug: true,
+      language: 'en',
+      resultCallback: async (result: HppTransactionResponse) => {
+        this.transactionStatus = result.order.status;
+        if (this.transactionStatus === PaymentStatus.SESSION_EXPIRED) {
+          await this.loadTransactionSetup();
+        }
+      },
+    });
+  }
+
+  private getAlertMessageContent(): JSX.Element | null {
+    const { paymentSuccessMessage, cardErrorMessage, sessionExpiredMessage, transactionSetupFailure } = CONSTANTS;
+    const status = this.transactionStatus;
+    const showAlert =
+      (status === PaymentStatus.SUCCESS ||
+        status === PaymentStatus.FAILURE ||
+        status === PaymentStatus.ERROR ||
+        status === PaymentStatus.EXCEPTION ||
+        status === PaymentStatus.SESSION_EXPIRED ||
+        (this.error && this.paymentMethod)) &&
+      this.transactionSetupResponse;
+
+    if (!showAlert) return null;
+
     let type = AlertMessageType.DANGER;
-    if (this.transactionStatus === PaymentStatus.SUCCESS) {
-      message = CONSTANTS.paymentSuccessMessage;
-      type = AlertMessageType.SUCCESS;
-    } else if (
-      this.transactionStatus === PaymentStatus.FAILURE ||
-      this.transactionStatus === PaymentStatus.ERROR ||
-      this.transactionStatus === PaymentStatus.EXCEPTION
-    ) {
-      message = CONSTANTS.cardErrorMessage;
-    } else if (this.transactionStatus === PaymentStatus.SESSION_EXPIRED) {
-      message = CONSTANTS.sessionExpiredMessage;
-    } else if (
-      (this.error || !this.transactionSetupResponse?.transactionSetupUrl) &&
-      this.paymentMethod
-    ) {
-      message = CONSTANTS.transactionSetupFailure;
-    } else {
-      return null;
+    let message = '';
+
+    switch (status) {
+      case PaymentStatus.SUCCESS:
+        type = AlertMessageType.SUCCESS;
+        message = paymentSuccessMessage;
+        break;
+      case PaymentStatus.SESSION_EXPIRED:
+        message = sessionExpiredMessage;
+        break;
+      case PaymentStatus.FAILURE:
+      case PaymentStatus.ERROR:
+      case PaymentStatus.EXCEPTION:
+        message = cardErrorMessage;
+        break;
+      default:
+        message = transactionSetupFailure;
+        break;
     }
 
     return (
-      <div class="pt-[12px] pb-4">
+      <div class="pt-3 pb-4">
         <common-alert type={type} message={message}></common-alert>
       </div>
     );
   }
 
-  private onHandleBillingAddressSubmit(e: any) {
-    this.billingAddress = e.detail;
+  private handleBillingAddressSubmit = (e: CustomEvent): void => {
+    this.billingAddress = JSON.stringify(e.detail);
     this.disabledPaymentMethod = false;
-  }
+  };
 
-  private onBillingAddressCopied(e: any) {
-    this.billingAddress = e.detail;
+  private handleBillingAddressCopied = (e: CustomEvent): void => {
+    this.billingAddress = JSON.stringify(e.detail);
     this.disabledPaymentMethod = false;
     this.hasSameBillingAddressChecked = true;
-  }
+  };
 
   render() {
     return (
       <div class="min-h-screen grid place-items-center content-start mt-4 px-3">
         <div
-          class={`sm:w-[449px] md:w-[449px] lg:w-[449px] xl:w-[449px] rounded-xl border-secondary-stroke  bg-white min-h-[660px] p-8 ${
+          class={`sm:w-[449px] bg-white border border-secondary-stroke rounded-xl min-h-[660px] p-8 ${
             this.loading && !this.error ? 'content-center' : 'content-top'
           }`}
         >
-          {this.getStatusAlertMessage()}
-          <h4 class="font-semibold text-2xl leading-[120%] pb-4">Checkout</h4>
+          {this.getAlertMessageContent()}
+
+          <h4 class="font-semibold text-2xl pb-4">Checkout</h4>
+
           <order-summary
             order-summary={this.orderSummary}
             total-amount={this.totalAmount}
           ></order-summary>
+
           <shipping-address
             shipping-address={this.shippingAddress}
             billing-address={this.billingAddress}
             hasBillingAddress={this.hasValidBillingAddress}
-            onBillingAddressCopied={(e) => this.onBillingAddressCopied(e)}
+            onBillingAddressCopied={this.handleBillingAddressCopied}
           ></shipping-address>
-          {!this.hasSameBillingAddressChecked &&
-            !this.hasValidBillingAddress && (
-              <billing-address
-                billing-address={this.billingAddress}
-                onHandleBillingAddressSubmit={(e) =>
-                  this.onHandleBillingAddressSubmit(e)
-                }
-              ></billing-address>
-            )}
-          <h4 class="font-semibold text-lg leading-[120%] py-4">
-            Payment methods
-          </h4>
+
+          {!this.hasSameBillingAddressChecked && !this.hasValidBillingAddress && (
+            <billing-address
+              billing-address={this.billingAddress}
+              onHandleBillingAddressSubmit={this.handleBillingAddressSubmit}
+            ></billing-address>
+          )}
+
+          <h4 class="font-semibold text-lg py-4">Payment methods</h4>
+
           <div class="grid gap-6">
-            <div
-              class={`grid grid-cols-1 items-center p-4 border border-gray-200 rounded-lg dark:border-gray-700 ${
-                this.transactionSetupResponse && 'border-2 border-gray-700'
-              }`}
-            >
+            <div class={`grid p-4 border rounded-lg ${this.transactionSetupResponse ? 'border-gray-700 border-2' : 'border-gray-200'}`}>
               <div class="flex items-center">
                 <input
                   id="card-payment-method"
                   type="radio"
-                  value={this.paymentMethod}
                   name="payment-method"
                   disabled={this.disabledPaymentMethod}
-                  onClick={() => {
-                    this.loadTransactionSetup();
-                  }}
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  onClick={() => this.loadTransactionSetup()}
+                  class="w-4 h-4 text-blue-600 border-gray-300 focus:ring-blue-500"
                 />
-                <label
-                  htmlFor="card-payment-method"
-                  class="flex justify-between items-center w-full ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  Credit or debit card{' '}
-                  <p class="text-right">
-                    <app-icon name="card-icon" class="text-right"></app-icon>
-                  </p>
+                <label htmlFor="card-payment-method" class="ml-2 text-sm font-medium">
+                  Credit or debit card <app-icon name="card-icon" class="ml-auto"></app-icon>
                 </label>
               </div>
               {this.loading && !this.error && (
@@ -241,25 +256,19 @@ export class PaymentProcessing {
                   <common-loader></common-loader>
                 </div>
               )}
-              {<div id="hpp-payment-component"></div>}
+              <div id="hpp-payment-component"></div>
             </div>
-            <div class="flex items-center p-4 border border-gray-200 rounded-lg dark:border-gray-700">
+
+            <div class="flex items-center p-4 border border-gray-200 rounded-lg">
               <input
                 id="ach-payment-method"
                 type="radio"
-                value={this.paymentMethod}
-                disabled={this.disabledPaymentMethod}
                 name="payment-method"
-                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                disabled={this.disabledPaymentMethod}
+                class="w-4 h-4 text-blue-600 border-gray-300 focus:ring-blue-500"
               />
-              <label
-                htmlFor="ach-payment-method"
-                class="flex justify-between items-center w-full ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                ACH
-                <p class="text-right">
-                  <app-icon name="building-icon" class="text-right"></app-icon>
-                </p>
+              <label htmlFor="ach-payment-method" class="ml-2 text-sm font-medium">
+                ACH <app-icon name="building-icon" class="ml-auto"></app-icon>
               </label>
             </div>
           </div>
